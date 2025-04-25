7f63f677b83f23b890d7112a97c83f1903aff3e3
SebastianPuruguay <sebastianpuruguay15@gmail.com>
2025-04-25 16:57:03 -0500

    redeploy2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/<jorgeproject>/'
+    ? '/JORGEPROJECT/'  // Cambia 'jorgeproject' por el nombre correcto
     : '/',
   devServer: {
     proxy: {
